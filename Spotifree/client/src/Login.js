58942789b0dd988858f4d90cc2f7b7696db48de6
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-03-19 20:02:51 +0800

    Created the Login button
    Able to redirect to auth url on click

---
+++ Spotifree/client/src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=1a9068e5a02c4897adb142f295edfe75&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+const Login = () => {
+    return (
+        <div className="login">
+            <a className="loginbtn"  href={AUTH_URL}>
+                Login with Spotify
+                </a>
+        </div>
+    );
+};
+
+export default Login;
